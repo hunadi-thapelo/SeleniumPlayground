5f04695d31c27f791c4e0edcb15554d932a3a4d5
hunadim <hmapulane@gmail.com>
2022-11-16 00:05:09 +0200

    dynamic testing Assignment06: completed

--- src/main/java/exerciseTests/dynamicTestingAssignment6.java
+++ src/main/java/exerciseTests/dynamicTestingAssignment6.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 
 
 public class dynamicTestingAssignment6 {
@@ -14,14 +15,25 @@ public class dynamicTestingAssignment6 {
 
         driver.get("https://www.rahulshettyacademy.com/AutomationPractice/");
 
-        WebElement checkboxOption = driver.findElement(By.id("checkBoxOption1"));
-        checkboxOption.click();
-        System.out.println("Test2 is "+checkboxOption.getAttribute("value"));
-        //String optionSelected = driver.findElement(By.id("checkBoxOption1")).getText();
-
+        //Test case 1: checkbox
+        WebElement checkBoxOption = driver.findElement(By.id("checkBoxOption3"));
+        checkBoxOption.click();
+        String checkboxText = checkBoxOption.getAttribute("value");
+        //System.out.println(checkboxText);
+        //Test case 2: dropdown select
         WebElement dropdown = driver.findElement(By.id("dropdown-class-example"));
         Select dropdownElement = new Select(dropdown);
-        dropdownElement.selectByVisibleText("Option2");
+        dropdownElement.selectByValue(checkboxText);
+        //Test case 3: Text box input
+        driver.findElement(By.id("name")).sendKeys(checkboxText);
+        driver.findElement(By.id("alertbtn")).click();
+        //Test case 4: Verify is text from test case 1 is present in pop up message
+        String alert = driver.switchTo().alert().getText();
+        String alertText = alert.split(",")[0].split("Hello")[1].trim();
+        //System.out.println(alertText);
+        Assert.assertEquals(alertText, "option3");
+        driver.switchTo().alert().accept();
+        driver.close();
 
 
 
