5018e0f3919bbf42a651291b40a8118545af2ec1
hunadim <hmapulane@gmail.com>
2022-11-25 22:51:53 +0200

    Proxy class: set capability to chrome behaviour

--- src/main/java/browserMiscTopics/httpsCert.java
+++ src/main/java/browserMiscTopics/httpsCert.java
@@ -1,5 +1,6 @@
 package browserMiscTopics;
 
+import org.openqa.selenium.Proxy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -9,6 +10,10 @@ public class httpsCert {
     {
 
         ChromeOptions options = new ChromeOptions();
+        Proxy proxies = new Proxy();
+        proxies.setHttpProxy("ipaddress:123.12.00.22");//example, set to pass the string here
+        options.setCapability("proxy", proxies); //setting capability of proxy to chrome behaviour
+
         options.setAcceptInsecureCerts(true); //behavior options object for browser
         System.setProperty("chromedriver", "usr/local/bin");
         WebDriver driver = new ChromeDriver(options); //chrome driver to get info on how to behave
