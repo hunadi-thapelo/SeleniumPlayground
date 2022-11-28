2d61b6ca92263bc6d8abb819f7943fef3d706450
hunadim <hmapulane@gmail.com>
2022-11-28 21:58:32 +0200

    Soft assertion to automate multiple broken links with Java method openConnection

--- src/main/java/browserMiscTopics/automateMultipleBrokenLinks.java
+++ src/main/java/browserMiscTopics/automateMultipleBrokenLinks.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
+import org.testng.asserts.SoftAssert;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
@@ -18,6 +18,8 @@ public class automateMultipleBrokenLinks {
         driver.get("https://www.rahulshettyacademy.com/AutomationPractice/");
 
         List<WebElement> links = driver.findElements(By.cssSelector("li[class='gf-li'] a"));
+        SoftAssert sa = new SoftAssert(); //to validate all links without failing execution when broken link is tested
+
         for(WebElement link: links){
 
             //Step1: Get URL tied up to the link
@@ -28,14 +30,16 @@ public class automateMultipleBrokenLinks {
             conn.connect(); //connect() method of conn object
             int responseCode= conn.getResponseCode(); //get url connection response code e.g 200, 404
             System.out.println(responseCode);
-            if(responseCode>400){
+            sa.assertTrue(responseCode<400,"Link with text " + link.getText() + " is broken with status code " + responseCode);
+            /*if(responseCode>400){
 
                 System.out.println("Link with text " + link.getText() + " is broken with status code " + responseCode);
                 Assert.assertTrue(false);
 
-            }
+            }*/
 
         }
+        sa.assertAll();
 
     }
 
