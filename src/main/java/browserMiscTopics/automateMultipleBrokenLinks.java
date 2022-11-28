1fdbb989686d5857ea3f97b5603abc72d31ebfec
hunadim <hmapulane@gmail.com>
2022-11-28 21:22:59 +0200

    automate multiple broken links with Java method openConnection and assertion

---
+++ src/main/java/browserMiscTopics/automateMultipleBrokenLinks.java
@@ -0,0 +1,42 @@
+package browserMiscTopics;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
+
+public class automateMultipleBrokenLinks {
+    public static void main(String[] args) throws IOException {
+        System.setProperty("chromedriver", "usr/local/bin");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.rahulshettyacademy.com/AutomationPractice/");
+
+        List<WebElement> links = driver.findElements(By.cssSelector("li[class='gf-li'] a"));
+        for(WebElement link: links){
+
+            //Step1: Get URL tied up to the link
+            String url = link.getAttribute("href");
+            //Step2: Java methods to call URLs and get status code
+            HttpURLConnection conn = (HttpURLConnection)new URL(url).openConnection();
+            conn.setRequestMethod("HEAD"); //using HEAD request http method
+            conn.connect(); //connect() method of conn object
+            int responseCode= conn.getResponseCode(); //get url connection response code e.g 200, 404
+            System.out.println(responseCode);
+            if(responseCode>400){
+
+                System.out.println("Link with text " + link.getText() + " is broken with status code " + responseCode);
+                Assert.assertTrue(false);
+
+            }
+
+        }
+
+    }
+
+}
